0d920927967f8e372e77d1158e917657974ee9b2
yeymiT <yeymicuellarm@gmail.com>
2026-06-12 15:34:47 -0500

    Creando rama burbuja y metodo sort

--- src/main/java/org/example/Busqueda.java
+++ src/main/java/org/example/Busqueda.java
@@ -14,5 +14,45 @@ public class Busqueda {
         }
         return -1;
     }
-    
+
+
+    // Método que realiza el ordenamiento burbuja
+    public void sort(int[] arr) {
+        int n = arr.length;
+        boolean intercambiado;
+
+        // Bucle externo para recorrer todo el arreglo
+        for (int i = 0; i < n - 1; i++) {
+            intercambiado = false;
+
+            // Bucle interno para comparar elementos adyacentes
+            // El "- i" evita revisar los últimos elementos que ya están ordenados
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Intercambiar arr[j] y arr[j+1]
+                    int temporal = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temporal;
+
+                    intercambiado = true; // Hubo un cambio
+                }
+            }
+
+            // Si no hubo intercambios en todo el bucle interno, el arreglo ya está ordenado
+            if (!intercambiado) {
+                break;
+            }
+        }
+    }
+
+    // Método auxiliar para imprimir el arreglo
+    public static void imprimirArreglo(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
 }
+
+
+
