e18727164bb9a38e64146a440ca5f3c0cd1e9d66
yeymiT <yeymicuellarm@gmail.com>
2026-06-12 15:15:21 -0500

    Creando rama busqueda_binaria y editando clase

--- src/main/java/org/example/Busqueda.java
+++ src/main/java/org/example/Busqueda.java
@@ -3,11 +3,16 @@ package org.example;
 public class Busqueda {
 
     public int buscar(int[] lista, int valor) {
-        for (int i = 0; i < lista.length; i++) {
-            if (lista[i] == valor) {
-                return i;
-            }
+
+        int bajo = 0;
+        int alto = lista.length - 1;
+        while (bajo <= alto) {
+            int medio = bajo = (alto - bajo) / 2;
+            if (lista[medio] == valor) return medio;
+            if (lista[medio] < valor) bajo = medio = 1;
+            else alto = medio - 1;
         }
         return -1;
     }
+    
 }
